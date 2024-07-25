d80ee4350dc232c01e31ceb3776935ee3d20fe5d
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-07-25 12:44:27 +0100

    Page snapping improvement

--- paging.js
+++ paging.js
@@ -1,7 +1,10 @@
+import { MathUtils } from "three";
+
 window.addEventListener('wheel', onWheel);
 window.requestAnimationFrame(update)
 
-const lastScrollCooldown = 250;
+const pageDistanceThreshold = 0.3;      // As a percentage of page height
+const lastScrollCooldown = 350;
 let lastScrollTime = 0;
 let prevTimestep = 0;
 
@@ -12,26 +15,28 @@ function onWheel(e) {
 function update(timestep) {
     const dt = timestep - prevTimestep;
 
-    updatePageSnapping();
+    updatePageSnapping(dt);
 
     prevTimestep = timestep;
 
     window.requestAnimationFrame(update);
 }
 
-function updatePageSnapping() {
-    const pageDistanceThreshold = 0.3;
+function updatePageSnapping(dt) {
     const timeSinceLastScroll = Date.now() - lastScrollTime;
 
     if (timeSinceLastScroll > lastScrollCooldown) {
         const pageHeight = window.innerHeight;
         const currentPageIndex = Math.round(window.scrollY / pageHeight);
-        const targetPageY = currentPageIndex * pageHeight;
+        const currentPageY = currentPageIndex * pageHeight;
 
         // Distance to target as a percentage of page height
-        const distanceToTarget = Math.abs((targetPageY - window.scrollY) / pageHeight);
+        const distanceToTarget = Math.abs((currentPageY - window.scrollY) / pageHeight);
+        const targetScrollY = distanceToTarget < pageDistanceThreshold ? currentPageY : window.scrollY;
+
         if (distanceToTarget < pageDistanceThreshold) {
-            window.scroll({ top: targetPageY, behavior: 'smooth' });
+            const targetScrollDelta = targetScrollY - window.scrollY;
+            window.scrollBy(0, Math.round(targetScrollDelta * 0.1 + 0.5));
         }
     }
 }
