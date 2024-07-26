64e1b2f48188cdec3faea67d21fd67580fbd1261
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-07-26 15:10:00 +0100

    Plane mesh for video texture and three.modifiers to modify it

--- src/scene3d.js
+++ src/scene3d.js
@@ -1,13 +1,22 @@
 import * as THREE from 'three';
 
+import { Bend, ModConstant, ModifierStack } from "three.modifiers";
 import Stats from 'three/addons/libs/stats.module.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 import { FontLoader, TextGeometry } from 'three/examples/jsm/Addons.js';
+import { MathUtils } from 'three/src/math/MathUtils.js';
 
 let renderer;
 let stats;
 let camera;
 let scene;
+let modifier;
+
+const bendModifier = new Bend(0, 0.3, 1);
+bendModifier.constraint = ModConstant.LEFT;
+
+const clock = new THREE.Clock(true);
 
 function init() {
     window.addEventListener('scroll', onScroll);
@@ -27,14 +36,13 @@ function init() {
     const frustum = frustumFromWindowWidth();
     const aspect = window.innerWidth / window.innerHeight;
     camera = new THREE.OrthographicCamera(frustum * aspect / - 2, frustum * aspect / 2, frustum / 2, frustum / - 2, 0.1, 1000);
-    camera.position.z = 100;
+    camera.position.z = 10;
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0.1, 0.1, 0.1, 1);
 
     new RGBELoader().setPath('assets/').load('quarry_01_1k.hdr', function (texture) {
         texture.mapping = THREE.EquirectangularReflectionMapping;
-        // scene.background = texture;
         scene.environment = texture;
     });
 
@@ -44,16 +52,45 @@ function init() {
         map: createVideoTexture(),
     });
 
-    // const torus = new THREE.Mesh(new THREE.TorusKnotGeometry(1, 0.3, 128, 16), material);
-    // torus.receiveShadow = true;
-    // torus.castShadow = true;
-    // scene.add(torus);
-
-    const cubeScale = 2;
-    const cube = new THREE.Mesh(new THREE.BoxGeometry(16 * cubeScale, 9 * cubeScale, 1, 4, 4, 4), material);
+    const cube = new THREE.Mesh(new THREE.BoxGeometry(1.6, 0.9, 0.1, 4, 4, 4), material);
+    cube.position.x -= 2;
     scene.add(cube);
 
-    createVideoTexture();
+    const light = new THREE.DirectionalLight(0xffffff, 4);
+    light.position.set(1, 1, 1).normalize();
+    scene.add(light);
+
+    const loader = new GLTFLoader();
+    loader.load(
+        'assets/video-plane.glb',
+        function (gltf) {
+            console.log("scene", gltf.scene);
+
+            gltf.scene.traverse((node) => {
+                if (node.isMesh) {
+                    node.material = material;
+
+                    if (modifier === undefined) {
+                        modifier = new ModifierStack(node);
+                    }
+                    else {
+                        throw "Modifier already defined";
+                    }
+
+                    modifier.addModifier(bendModifier);
+                }
+                console.log(node)
+            });
+
+            scene.add(gltf.scene);
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        function (error) {
+            console.error("Error", error);
+        }
+    );
 }
 
 function add3dText(text) {
@@ -100,7 +137,7 @@ function createVideoTexture() {
 function frustumFromWindowWidth() {
     // Pick an arbitrary width and frustum. Every other window width will be based off this
     const calibrationWidth = 1280;
-    const calibrationFrustum = 75;
+    const calibrationFrustum = 15;
 
     const scale = calibrationWidth / window.innerWidth;
     return calibrationFrustum * scale;
@@ -116,18 +153,24 @@ function onWindowResized() {
     camera.top = frustum / 2;
     camera.bottom = - frustum / 2;
     camera.updateProjectionMatrix();
-    
+
     console.log("New frustum: ", frustum);
 }
 
 function onScroll() {
     const scrollProgress = window.scrollY / (document.body.clientHeight);
-    camera.position.y = -scrollProgress * 200;
+    camera.position.y = -scrollProgress * 100;
+
 }
 
 function animate() {
+    const t = clock.getElapsedTime() % 1;
+
     renderer.render(scene, camera);
     stats.update();
+
+    bendModifier.force = Math.sin(clock.getElapsedTime());
+    modifier && modifier.apply();
 }
 
 init();
