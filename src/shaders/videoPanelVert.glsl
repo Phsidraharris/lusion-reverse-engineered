7b631910419d71f4def5bab68060f6ae34f90dc6
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-09-08 21:35:53 +0100

    Lerp between video panel start and end positions

--- src/shaders/videoPanelVert.glsl
+++ src/shaders/videoPanelVert.glsl
@@ -21,14 +21,27 @@ vec2 rotate(vec2 pos, float angle) {
     return pos;
 }
 
+// Takes VideoPanelShader rect (see VideoPanelShader.rectToVec4) and creates a position based on percentage a
+vec2 getRectPos(vec4 rect, vec2 uv) {
+    vec2 pos;
+    pos.x = mix(rect.x, rect.x + rect.w, uv.x);
+    pos.y = mix(rect.y, rect.y + rect.z, uv.y);
+    return pos;
+}
+
 void main() {
     vec3 pos = position;
 
     float stepEdge = 1.0 - sin(maskProgress * PI) * 3.;   // multiply by 3 so that when maskProgress = 1, step edge contains more of the uv
-    
+
     vUv = uv;
     vMask = smoothstep(stepEdge, 1.0, uv.x);
     vMask *= smoothstep(stepEdge, 1.0, uv.y);
+
+    vec2 videoPanelStartPos = getRectPos(startRect, uv);
+    vec2 videoPanelEndPos = getRectPos(endRect, uv);
+
+    pos.xy = mix(videoPanelStartPos, videoPanelEndPos, maskProgress);
 
     pos.xy += vec2(0.1, 0.1) * vMask;
     pos.xy = rotate(pos.xy, PI * 0.125 * vMask);
