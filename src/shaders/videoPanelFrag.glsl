d0822f59809b1f9e657d9a73621971d1c4ff2485
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-09-09 13:56:25 +0100

    Rounded corner with plane aspect wip

--- src/shaders/videoPanelFrag.glsl
+++ src/shaders/videoPanelFrag.glsl
@@ -1,16 +1,29 @@
 uniform sampler2D map;
 uniform float borderRadius;
+uniform vec4 startRect;
+uniform vec4 endRect;
+uniform float maskProgress;
 
 varying vec2 vUv;
+
+float getAspect() {
+    float width = mix(startRect.w, endRect.w, maskProgress);
+    float height = mix(startRect.z, endRect.z, maskProgress);
+    return width / height;
+}
 
 float roundedCornerMask() {
     // Convert to normalised device coordinates (i.e center = 0, 0)
     vec2 uv = vUv * 2.0 - 1.0;
+    uv = abs(uv);
 
     // Define the position in normalized space
-    vec2 cornerDistance = max(abs(uv) - (vec2(1.0) - borderRadius), 0.0);
+    vec2 cornerDistance = max(uv - (vec2(1.0) - borderRadius), 0.0);
+    // cornerDistance.x *= getAspect();
+    
     float dist = length(cornerDistance);
 
+    return step(borderRadius, dist);
     // Define alpha based on distance and borderRadius
     float alpha = step(0., borderRadius - dist);
 
@@ -19,5 +32,5 @@ float roundedCornerMask() {
 
 void main() {
     vec4 albedo = texture2D(map, vUv);
-    gl_FragColor = albedo * roundedCornerMask();
+    gl_FragColor = albedo + roundedCornerMask();
 }
