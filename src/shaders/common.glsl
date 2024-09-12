76cbbfd7e786852fe0811a1d455e46e55b6c4753
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-09-12 23:04:47 +0100

    Wip taper effect

--- src/shaders/common.glsl
+++ src/shaders/common.glsl
@@ -1,12 +1,16 @@
-float roundedCornerMask(vec2 uv, float borderRadius, float aspect) {
+vec2 getNdcUV(vec2 uv) {
+    return uv * 2.0 - 1.0;
+}
+
+float roundedCornerMask(vec2 uv, float borderRadius, float aspect, float taper) {
     // Convert to normalised device coordinates (i.e center = 0, 0),
     // then treat corners as distances from the center with abs
-    vec2 uv_ndc = uv * 2.0 - 1.0;
+    vec2 uv_ndc = getNdcUV(uv);
     uv_ndc = abs(uv_ndc);
 
     // distance to corner
     float inverseAspect = 1. / aspect;
-    vec2 corner = uv_ndc - vec2(1.0 - borderRadius * inverseAspect, 1.0 - borderRadius);
+    vec2 corner = uv_ndc - vec2(1.0 - borderRadius * inverseAspect - taper, 1.0 - borderRadius);
 
     // max 0.0, ignores negative values (i.e all fragments inside the rounded box)
     corner = max(corner, vec2(0.0, 0.0));
@@ -19,4 +23,8 @@ float roundedCornerMask(vec2 uv, float borderRadius, float aspect) {
     // // Define alpha based on distance and borderRadius
     // float alpha = step(0., borderRadius - dist);
     // return alpha;
+}
+
+float roundedCornerMask(vec2 uv, float borderRadius, float aspect) {
+    return roundedCornerMask(uv, borderRadius, aspect, 0.0);
 }
--- src/shaders/projectTileFrag.glsl
+++ src/shaders/projectTileFrag.glsl
@@ -1,6 +1,7 @@
 #include "./common.glsl"
 
 uniform float aspect;
+uniform float taperAmount;
 uniform sampler2D map;
 
 varying vec2 vUv;
@@ -8,7 +9,8 @@ varying vec2 vUv;
 void main() {
     vec4 albedo = texture2D(map, vUv);
     
-    albedo.a = roundedCornerMask(vUv, 0.1, aspect);
+    float taperScaled = taperAmount * vUv.y;
+    albedo.a = roundedCornerMask(vUv, 0.1, aspect, taperScaled);
 
     gl_FragColor = albedo;
 }
