2145605d630ab2f0a5f7ac92012b0c3e5c58773e
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-09-14 23:04:27 +0100

    Normalize device coordinate and scale uv by aspect to get consistent sizing across screen aspects

--- src/shaders/loadingMeshFrag.glsl
+++ src/shaders/loadingMeshFrag.glsl
@@ -1,19 +1,26 @@
+#include "./common.glsl"
+
 uniform float aspect;
 
 varying vec2 vUv;
 
-float calculateRect(vec2 bl, vec2 tr) {
+float calculateRect(vec2 bl, vec2 tr, vec2 uv) {
     float rect = 1.0;
-    rect -= step(vUv.x, bl.x);
-    rect -= step(tr.x, vUv.x);
-    rect -= step(vUv.y, bl.y);
-    rect -= step(tr.y, vUv.y);
+    rect -= step(uv.x, bl.x);
+    rect -= step(tr.x, uv.x);
+    rect -= step(uv.y, bl.y);
+    rect -= step(tr.y, uv.y);
 
     return rect;
 }
 
 void main() {
     vec4 col = vec4(0., 0., 0., 1.0);
+
+    vec2 ndcUv = getNdcUV(vUv);
+    ndcUv.x *= aspect;
+    // these uvs run from -1 to 1
+    // how can we make these run from 
 
     // the start and end points of the rect that make up the vertical
     // Because UVs are defined from bottom left to bottom right, that how we'll define our rect corners
@@ -23,8 +30,8 @@ void main() {
     vec2 lHorizontalTr = vec2(0.7, 0.5);
 
     // remove everything outside the defined rectangle
-    float vertical = calculateRect(lVerticalBl, lVerticalTr);
-    float horizontal = calculateRect(lHorizontalBl, lHorizontalTr);
+    float vertical = calculateRect(lVerticalBl, lVerticalTr, ndcUv);
+    float horizontal = calculateRect(lHorizontalBl, lHorizontalTr, ndcUv);
     float combined = step(1., vertical) + step(1., horizontal);
 
     if(combined > 0.0) {
