e83a409d14306ae807357475b071ce3a67ffac30
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-09-20 20:10:28 +0100

    Remove redundancy

--- src/shaders/projectTileVert.glsl
+++ src/shaders/projectTileVert.glsl
@@ -5,11 +5,9 @@ varying vec2 vUv;
 void main() {
     vec3 newPosition = position;
 
-    // convert uv to NDC
-    vec2 ndcUv = uv * 2.0 - 1.0;
-    float direction = sign(stretchAmount);
+    float ndcUvY = uv.y * 2.0 - 1.0;
 
-    newPosition.x *= 1. + (direction * sin(ndcUv.y * abs(stretchAmount)));
+    newPosition.x *= 1. + (ndcUvY * stretchAmount);
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
     vUv = uv;
