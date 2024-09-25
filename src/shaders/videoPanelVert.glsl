b84cb891c565e0a5aea3e4a5ed8f22519ecf5f71
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-09-25 22:56:51 +0100

    Distinct rotate, translate and mix between start/end positions

--- src/shaders/videoPanelVert.glsl
+++ src/shaders/videoPanelVert.glsl
@@ -22,15 +22,21 @@ void main() {
     float stepEdgeCurve = 1.0 - sin(animateProgress * PI) * 2.0;    // multiply by 3 so that when animateProgress = 1, step edge contains more of the uv
     float startEndCurve = smoothstep(0.2, 1.0, animateProgress);
     float rotateCurve = PI * 0.125 * sin(animateProgress * PI);     // rotate by 1/8th of PI (i.e 16th turn of a circle), that peaks at animateProgress = 0.5
+    float translateCurve = smoothstep(0.0, 0.2, animateProgress) - smoothstep(0.2, 0.8, animateProgress);
 
     vec2 videoPanelStartPos = getRectPos(startRect, uv);
     vec2 videoPanelEndPos = getRectPos(endRect, uv);
 
-    float mask = smoothstep(stepEdgeCurve, 1.0, uv.x);
-    mask *= smoothstep(stepEdgeCurve, 1.0, uv.y);
+    float rotateMask = smoothstep(stepEdgeCurve, 1.0, uv.x);
+    rotateMask *= smoothstep(stepEdgeCurve, 1.0, uv.y);
+
+    float translateMask = smoothstep(0.6, 1.0, uv.x);
+    translateMask *= smoothstep(0.5, 1.0, uv.y);
+    translateMask *= translateCurve;
 
     pos.xy = mix(videoPanelStartPos, videoPanelEndPos, startEndCurve);
-    pos.xy = rotate(pos.xy, rotateCurve * mask);
+    pos.xy = rotate(pos.xy, rotateCurve * rotateMask);
+    pos.x *= 1.0 + 0.4 * translateMask;
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 
