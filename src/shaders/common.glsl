f82b60afdaf4c4cd8a28e30fb871db4d1fa5e71a
Mark Nguyen <1138440+canxerian@users.noreply.github.com>
2024-09-27 12:32:10 +0100

    Simplified rounded corner mask function and added comments

--- src/shaders/common.glsl
+++ src/shaders/common.glsl
@@ -4,25 +4,34 @@ vec2 getNdcUV(vec2 uv) {
 
 float roundedCornerMask(vec2 uv, float borderRadius, float aspect, float taper) {
     // Convert to normalised device coordinates (i.e center = 0, 0),
-    // then treat corners as distances from the center with abs
     vec2 uv_ndc = getNdcUV(uv);
+
+    // then treat corners as distances from the center with abs
     uv_ndc = abs(uv_ndc);
 
-    // distance to corner
-    float inverseAspect = 1. / aspect;
-    vec2 corner = uv_ndc - vec2(1.0 - borderRadius * inverseAspect - taper, 1.0 - borderRadius);
+    // If we didn't include the mask taper, we could simply do this: (easier to explain in the video, perhaps?)
+    // vec2 corner = uv_ndc - (1.0 - borderRadius);
 
-    // max 0.0, ignores negative values (i.e all fragments inside the rounded box)
+    // For the current uv, which now ranges from 0 at the center to 1 at the edges,
+    // subtract 1 - borderRadius, such that everything inside the border has a value of 0 or less
+    // and everything else (e.g the bits we want to not render) have a value > 0 
+
+    vec2 corner;
+    corner.x = uv_ndc.x - (1.0 - borderRadius - taper);
+    corner.y = uv_ndc.y - (1.0 - borderRadius);
+
+    // Now we say the max value can be 0.0, so anything above is truncated, this is also known as "clipping"
     corner = max(corner, vec2(0.0, 0.0));
     corner.x *= aspect;
 
-    float dist = length(corner);
+    // Clever little trick to convert vec2 values in a float. This does two things: let's us treat negative values as positive
+    // then tells us how far from the corner this fragment is.
+    // in the line above we set all the corner values to 0, so they'lll remain zero.
+    float distanceFromCorner = length(corner);
 
-    return step(dist, borderRadius);
-
-    // // Define alpha based on distance and borderRadius
-    // float alpha = step(0., borderRadius - dist);
-    // return alpha;
+    // Now we return 0 if distanceFromCorner is less than borderRadius.
+    // And return 1 for everything equal or greater - that's everything inside the border
+    return step(distanceFromCorner, borderRadius);
 }
 
 float roundedCornerMask(vec2 uv, float borderRadius, float aspect) {
